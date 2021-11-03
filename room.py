a8453f0448b33d8c049b133aafe007c74fc55a5d
Jay-Salunke <jysl2004@gmail.com>
2021-11-03 13:20:32 +0530

    Added the left side frame and added three entry types name,checkin date,checkout date in room.py file

--- room.py
+++ room.py
@@ -1,5 +1,8 @@
-from os import name
 from tkinter import *
+from tkinter import ttk
+from tkinter import font
+from PIL import Image, ImageTk
+from tkcalendar import DateEntry
 
 
 class Room:
@@ -8,6 +11,68 @@ class Room:
         self.root = root
         self.root.geometry('890x385+430+200')
         self.root.title('Room booking')
+
+        ################################### CUSTOMER DETAIL TITLE###################################
+        customer_details_title = Label(self.root, bg="black", fg="gold", text="ADD ROOM BOOKING DETAILS",
+                                       padx=360, pady=15, font=("new timed roman", 12, "bold"))
+        customer_details_title.place(x=0, y=0)
+        ############################################################################################
+
+        ############################## LEFT SIDE LOGO IMAGE #######################################
+        hotel_logo = Image.open(
+            'D:\python_tkinter_project\images\golden-hotel-logo-free-graphics-472233.jpg')
+        hotel_logo = hotel_logo.resize((60, 50), Image.ANTIALIAS)
+        self.hotel_logo1 = ImageTk.PhotoImage(hotel_logo)
+        hotel_logo1_label = Label(
+            self.root, image=self.hotel_logo1, relief=RIDGE)
+        hotel_logo1_label.place(x=0, y=0)
+        ############################################################################################
+        
+        ######################################LEFT SIDE FRAME #######################################
+        left_side_frame = LabelFrame(
+            self.root, text='ADD ROOM BOOKING DETAILS', relief=RIDGE, font=("new times roman", 11, "bold"))
+        left_side_frame.place(x=2, y=54, width=385, height=330)
+        
+        ##################################### ADD DETAIL ENTRIES ####################################
+
+        # customer name
+        name_lbl = ttk.Label(left_side_frame, text="Customer contact no.", font=(
+            "new times roman", 9, "bold"))
+        name_lbl.place(x=2, y=2)
+
+        name_entry = ttk.Entry(left_side_frame, font=(
+            "new times roman", 9, "bold"),)
+        name_entry.place(x=5, y=20, width=160)
+        
+        #check in date entry
+        check_in_lbl = ttk.Label(left_side_frame, text='Check in date', font=(
+            "new times roman", 9, "bold"))
+        check_in_lbl.place(x=190, y=2)
+
+        check_in_date_entry = DateEntry(
+            left_side_frame, font=("new times roman", 9, "bold"),selectmode = 'day')
+        check_in_date_entry.place(x=190, y=20, width=160)
+
+        #check out date entry
+        check_out_lbl = ttk.Label(left_side_frame,text="Check out date",font=("new times roman",9,"bold"))
+        check_out_lbl.place(x=2, y=60)
+
+        check_out_date_entry = DateEntry(left_side_frame,selectmode = 'day',font=("new times roman",9,"bold"))
+        check_out_date_entry .place(x=5, y=78, width=160)
+
+        #room type combobox
+        room_type_lbl= ttk.Label(
+            left_side_frame, text="Room type", font=("new times roman", 9, "bold"))
+        room_type_lbl.place(x=190, y=60)
+
+        room_type_combo_box= ttk.Combobox(
+            left_side_frame, font=("new times roman", 9, "bold"), width=160)
+        room_type_combo_box.place(x=190,y=78,width=160)
+        
+
+           
+
+        ############################################################################################
 
 
 if __name__ == '__main__':
